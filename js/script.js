2e0f04d59434423d786c91d6a3daa55d94d8006d
Lloyd Lobo <lloyd@lloydlobo.com>
2022-02-27 08:18:07 +0530

    Update critical script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 const critical = require("critical");
+
+critical.generate({
+  // Inline the generated critical-path CSS
+  // - true generates HTML
+  // - false generates CSS
+  inline: true,
+
+  // Your base directory
+  base: "dist/",
+
+  // HTML source
+  html: "<html>...</html>",
+
+  // HTML source file
+  src: "index.html",
+
+  // Your CSS Files (optional)
+  css: ["dist/style.css"],
+
+  // Viewport width
+  width: 1300,
+
+  // Viewport height
+  height: 900,
+
+  // Output results to file
+  target: {
+    css: "critical.css",
+    html: "index-critical.html",
+    uncritical: "uncritical.css",
+  },
+
+  // Extract inlined styles from referenced stylesheets
+  extract: true,
+
+  // ignore CSS rules
+  ignore: {
+    atrule: ["@font-face"],
+    rule: [/some-regexp/],
+    decl: (node, value) => /big-image\.png/.test(value),
+  },
+});
