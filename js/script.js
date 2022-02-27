ad30ef4a3bf91247bfa2829796b4bb06f0566be4
Lloyd Lobo <lloyd@lloydlobo.com>
2022-02-27 23:12:48 +0530

    Feat: Responsive mobile menu on click

--- js/script.js
+++ js/script.js
@@ -1,43 +1,62 @@
-const critical = require("critical");
+//  hamburger menu functionality
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-critical.generate({
-  // Inline the generated critical-path CSS
-  // - true generates HTML
-  // - false generates CSS
-  inline: true,
-
-  // Your base directory
-  base: "dist/",
-
-  // HTML source
-  html: "<html>...</html>",
-
-  // HTML source file
-  src: "index.html",
-
-  // Your CSS Files (optional)
-  css: ["dist/style.css"],
-
-  // Viewport width
-  width: 1300,
-
-  // Viewport height
-  height: 900,
-
-  // Output results to file
-  target: {
-    css: "critical.css",
-    html: "index-critical.html",
-    uncritical: "uncritical.css",
-  },
-
-  // Extract inlined styles from referenced stylesheets
-  extract: true,
-
-  // ignore CSS rules
-  ignore: {
-    atrule: ["@font-face"],
-    rule: [/some-regexp/],
-    decl: (node, value) => /big-image\.png/.test(value),
-  },
+// create 'data attributes' in index.html to toggle
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+  console.log(visibility);
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+  }
+  console.log(visibility);
 });
+
+// //
+// // bot stuff - github integration
+// //
+// const critical = require("critical");
+
+// critical.generate({
+//   // inline the generated critical-path CSS
+//   // - true generates HTML
+//   // - false generates CSS
+//   inline: true,
+
+//   // Your base directory
+//   base: "dist/",
+
+//   // HTML source
+//   html: "<html>...</html>",
+
+//   // HTML source file
+//   src: "index.html",
+
+//   // Your CSS Files (optional)
+//   css: ["dist/style.css"],
+
+//   // Viewport width
+//   width: 1300,
+
+//   // Viewport height
+//   height: 900,
+
+//   // Output results to file
+//   target: {
+//     css: "critical.css",
+//     html: "index-critical.html",
+//     uncritical: "uncritical.css",
+//   },
+
+//   // Extract inlined styles from referenced stylesheets
+//   extract: true,
+
+//   // ignore CSS rules
+//   ignore: {
+//     atrule: ["@font-face"],
+//     rule: [/some-regexp/],
+//     decl: (node, value) => /big-image\.png/.test(value),
+//   },
+// });
