525f09e01554fb84d75325e6f6f09c3c728bc56e
Lloyd Lobo <lloyd@lloydlobo.com>
2022-02-28 20:39:04 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-// create 'data attributes' in index.html to toggle
-navToggle.addEventListener("click", () => {
-  const visibility = primaryNav.getAttribute("data-visible");
-  // console.log(visibility);
-  if (visibility === "false") {
-    primaryNav.setAttribute("data-visible", true);
-  } else if (visibility === "true") {
-    primaryNav.setAttribute("data-visible", false);
-  }
-  // console.log(visibility);
-});
+//
+
+// // create 'data attributes' in index.html to toggle
+// navToggle.addEventListener("click", () => {
+//   const visibility = primaryNav.getAttribute("data-visible");
+//   if (visibility === "false") {
+//     primaryNav.setAttribute("data-visible", true);
+//   } else if (visibility === "true") {
+//     primaryNav.setAttribute("data-visible", false);
+//   }
+// });
 
 // //
 // // bot stuff - github integration
