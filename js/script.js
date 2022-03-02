24d053eb01f173ea53b139ec968b4aa46b20d28c
Lloyd Lobo <lloyd@lloydlobo.com>
2022-03-02 12:40:00 +0530

    refactor: :fire: Refactor: Remove "Critical" inline css package

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 //
 
 // create 'data attributes' in index.html to toggle
+// Element.setCapture() is deprecated. Use Element.setPointerCapture() instead.
 navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
   if (visibility === "false") {
@@ -13,50 +14,3 @@ navToggle.addEventListener("click", () => {
     primaryNav.setAttribute("data-visible", false);
   }
 });
-
-// //
-// // bot stuff - github integration
-// //
-// const critical = require("critical");
-
-// critical.generate({
-//   // inline the generated critical-path CSS
-//   // - true generates HTML
-//   // - false generates CSS
-//   inline: true,
-
-//   // Your base directory
-//   base: "dist/",
-
-//   // HTML source
-//   html: "<html>...</html>",
-
-//   // HTML source file
-//   src: "index.html",
-
-//   // Your CSS Files (optional)
-//   css: ["dist/style.css"],
-
-//   // Viewport width
-//   width: 1300,
-
-//   // Viewport height
-//   height: 900,
-
-//   // Output results to file
-//   target: {
-//     css: "critical.css",
-//     html: "index-critical.html",
-//     uncritical: "uncritical.css",
-//   },
-
-//   // Extract inlined styles from referenced stylesheets
-//   extract: true,
-
-//   // ignore CSS rules
-//   ignore: {
-//     atrule: ["@font-face"],
-//     rule: [/some-regexp/],
-//     decl: (node, value) => /big-image\.png/.test(value),
-//   },
-// });
